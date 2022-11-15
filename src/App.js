50c4df53aab6d0a9d288ac8bcb6f75dc093860c4
nsonhai1.1 <nsonhai1.1@gmail.com>
2022-11-15 19:37:42 +0700

    something

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./modules/Authentication/pages/Login";
 import Register from "./modules/Authentication/pages/Register";
 import MainLayout from "./modules/Home/MainLayout";
 import Home from "./modules/Home/Page/Home";
+import RoomDetail from "./modules/RoomDetail/pages/RoomDetail/RoomDetail";
 import RoomList from "./modules/RoomList/pages/RoomList/RoomList";
 
 function App() {
@@ -12,9 +13,8 @@ function App() {
          <Route path="/" element={<MainLayout />}>
             <Route index element={<Home />} />
             <Route path="room-list/:id" element={<RoomList />} />
+            <Route path="room-detail/:id" element={<RoomDetail />} />
          </Route>
-
-
 
          <Route path="/" element={<AuthLayout />}>
             <Route path="/login" element={<Login />} />
--- src/modules/RoomList/components/RoomListLocation/RoomListLocation.jsx
+++ src/modules/RoomList/components/RoomListLocation/RoomListLocation.jsx
@@ -1,7 +1,7 @@
 import { Col, Image, Row, Space } from "antd";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { AiOutlineHeart } from 'react-icons/ai';
+import { Link, useParams } from "react-router-dom";
+import { AiOutlineHeart } from "react-icons/ai";
 import roomAPI from "../../../../apis/roomAPI";
 import "./RoomListLocationStyles.scss";
 
@@ -15,7 +15,7 @@ const RoomListLocation = () => {
       setRoomListLocation(data);
       console.log("dataSource", roomListLocation);
    };
-	 
+
    useEffect(() => {
       getRoomList(id);
    }, []);
@@ -31,40 +31,81 @@ const RoomListLocation = () => {
                         <hr />
                         <Row gutter={[32, 16]}>
                            <Col span={10}>
-                              <Image src={room.hinhAnh} className="rounded" />
+                              <Image
+                                 src={room.hinhAnh}
+                                 className="rounded"
+                                 // width={"100%"}
+                                 height={"100%"}
+                              />
                            </Col>
                            <Col span={14}>
                               <div className="mb-3 room-title">
-                                 <h5>{room.tenPhong}</h5>
-																 <span className="icon-heart">
-																	 <AiOutlineHeart/>
-																 </span>
+                                 <Link to={`/room-detail/${room.id}`}>
+                                    {room.tenPhong}
+                                 </Link>
+                                 <span className="icon-heart">
+                                    <AiOutlineHeart />
+                                 </span>
                               </div>
                               <div className="mb-3">
                                  <p className="w-75">{room.moTa}</p>
                               </div>
                               <div className="text-secondary">
-                                 {room.khach && <span className="me-2">{room.khach} Khách</span>}
-                                 {room.giuong && <span className="me-2">{room.giuong} Giường</span>}
-                                 {room.phongNgu && <span className="me-2">{room.phongNgu} Phòng ngủ</span>}
-                                 {room.phongTam && <span>{room.phongTam} Phòng tắm</span>}
+                                 {room.khach && (
+                                    <span className="me-2">
+                                       {room.khach} Khách
+                                    </span>
+                                 )}
+                                 {room.giuong && (
+                                    <span className="me-2">
+                                       {room.giuong} Giường
+                                    </span>
+                                 )}
+                                 {room.phongNgu && (
+                                    <span className="me-2">
+                                       {room.phongNgu} Phòng ngủ
+                                    </span>
+                                 )}
+                                 {room.phongTam && (
+                                    <span>{room.phongTam} Phòng tắm</span>
+                                 )}
                               </div>
-															
-															
+
                               <div className="text-secondary mb-3">
-                                 {room.mayGiat && <span className="me-2">Máy giặt</span>}
-                                 {room.banLa && <span className="me-2">Bàn là</span>}
-                                 {room.tivi && <span className="me-2">Tivi</span>}
-                                 {room.dieuHoa && <span className="me-2">Điều hòa</span>}
-                                 {room.wifi && <span className="me-2">Wifi</span>}
-                                 {room.bep &&<span className="me-2"> Bếp</span>}
-                                 {room.doXe && <span className="me-2">Đỗ xe</span>}
-                                 {room.hoBoi && <span className="me-2">Hồ bơi</span>}
-                                 {room.banUi && <span className="me-2">Bàn ủi</span>}
+                                 {room.mayGiat && (
+                                    <span className="me-2">Máy giặt</span>
+                                 )}
+                                 {room.banLa && (
+                                    <span className="me-2">Bàn là</span>
+                                 )}
+                                 {room.tivi && (
+                                    <span className="me-2">Tivi</span>
+                                 )}
+                                 {room.dieuHoa && (
+                                    <span className="me-2">Điều hòa</span>
+                                 )}
+                                 {room.wifi && (
+                                    <span className="me-2">Wifi</span>
+                                 )}
+                                 {room.bep && (
+                                    <span className="me-2"> Bếp</span>
+                                 )}
+                                 {room.doXe && (
+                                    <span className="me-2">Đỗ xe</span>
+                                 )}
+                                 {room.hoBoi && (
+                                    <span className="me-2">Hồ bơi</span>
+                                 )}
+                                 {room.banUi && (
+                                    <span className="me-2">Bàn ủi</span>
+                                 )}
                               </div>
                               <div className="text-end fs-4">
-                                 <span className="fw-bold"> ${room.giaTien} </span>
-																 <span>/ tháng</span>
+                                 <span className="fw-bold">
+                                    {" "}
+                                    ${room.giaTien}{" "}
+                                 </span>
+                                 <span>/ tháng</span>
                               </div>
                            </Col>
                         </Row>
